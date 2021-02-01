8b25b46f5ace79d3d5c65a1ea41ace43ea05a4de
reubber <reubbers@alu.ufc.br>
2021-02-01 02:23:38 -0300

    Revert "add rated quizes and animations"
    This reverts commit d6850bc88405acc253a56740dfe5b3f3943acfde.

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import QuizBackground from '@/components/QuizBackground'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
 import QuizContainer from '@/components/QuizContainer'
+import { motion } from 'framer-motion'
+import Link from '@/components/Link'
 
 export default function Home() {
   const router = useRouter()
@@ -38,7 +40,16 @@ export default function Home() {
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' }
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
@@ -64,14 +75,52 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 }
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>FINNISH HIM</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map(linkExterno => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.')
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                )
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 1, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, x: '0' },
+            hidden: { opacity: 0, x: '100%' }
+          }}
+          initial="hidden"
+          animate="show"
+        ></Footer>
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/reubber" />
     </QuizBackground>
